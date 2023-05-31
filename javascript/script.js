a3f4a31ea183c3334f072f676de1117b575ec919
IsaSadigovDV <isans@code.edu.az>
2023-05-31 13:58:10 +0400

    creators html file created

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,9 @@
 const discoverCards = document.querySelector(".discover_cards");
 const discoverBtn = document.querySelector(".discoverBtn");
 
+const heartBtn = document.querySelector(".heart");
+const numberValue = document.querySelector(".number_value");
+
 discoverCards.addEventListener("click", () => {
   window.location.href = "../Html/artistPage.html";
 });
@@ -8,3 +11,7 @@ discoverCards.addEventListener("click", () => {
 discoverBtn.addEventListener("click", () => {
   window.location.href = "../Html/artistPage.html";
 });
+
+// heartBtn.addEventListener("click", () => {
+//   heartBtn.style.fill = "red";  
+// });
