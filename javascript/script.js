2d4f3e4e863e01c21f5d69c57e6f4733787b9148
IsaSadigovDV <isans@code.edu.az>
2023-05-31 08:59:55 +0400

    click artistPage event added

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const discoverCards = document.querySelector(".discover_cards");
+const discoverBtn = document.querySelector(".discoverBtn");
+
+discoverCards.addEventListener("click", () => {
+  window.location.href = "../Html/artistPage.html";
+});
+
+discoverBtn.addEventListener("click", () => {
+  window.location.href = "../Html/artistPage.html";
+});
