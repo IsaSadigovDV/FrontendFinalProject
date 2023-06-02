b2e7f8e3d62d7be2583183257b6c775d010633b2
IsaSadigovDV <isans@code.edu.az>
2023-06-02 11:37:52 +0400

    createAccount  responsive design added

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,13 @@
 const discoverCards = document.querySelector(".discover_cards");
 const discoverBtn = document.querySelector(".discoverBtn");
 
-
-
 const heartBtn = document.querySelector(".heart");
 const numberValue = document.querySelector(".number_value");
+
+// const burgerMenu = document.querySelector(".burgerMenu");
+// burgerMenu.addEventListener("click", () => {
+//   burgerMenu.style.width = "250px";
+// });
 
 discoverCards.addEventListener("click", () => {
   window.location.href = "../Html/artistPage.html";
