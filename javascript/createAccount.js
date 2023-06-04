0fc98acddd215177ac0dc2cf7cd7618df84f3a4f
IsaSadigovDV <isans@code.edu.az>
2023-06-04 23:06:50 +0400

    js validation added

--- javascript/createAccount.js
+++ javascript/createAccount.js
@@ -1,40 +1,103 @@
-const createInputs=document.querySelectorAll(".inputs>input");
-const vali1=document.querySelector(".vali1")
-const vali2=document.querySelector(".vali2")
-const vali3=document.querySelector(".vali3")
-const vali4=document.querySelector(".vali4")
-
-createInputs[0].addEventListener("keyup",()=>{
-    if(createInputs[0].value.trim()===null){
-        vali1.textContent="Plese enter username correctly";
-        vali1.style.color="red";
-    }
-})
-createInputs[1].addEventListener("keyup",()=>{
-    if(!ValidateEmail(createInputs[1].value)){
-        vali2.textContent="Please enter valid email";
-        vali2.style.color="red";
-    }
-})
-createInputs[2].addEventListener("keyup",()=>{
-    
-    
-})
-createInputs[3].addEventListener("keyup",()=>{
-   
-})
-
-
-
-
-
-
-
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(mail))
-  {
-    return (true)
+const createInputs = document.querySelectorAll(".inputs>input");
+const vali1 = document.querySelector(".vali1");
+const vali2 = document.querySelector(".vali2");
+const vali3 = document.querySelector(".vali3");
+const vali4 = document.querySelector(".vali4");
+const vali5 = document.querySelector(".vali5");
+const rightbottom = document.querySelector(".right_bottom");
+vali1.style.fontSize = "16px";
+vali2.style.fontSize = "16px";
+vali3.style.fontSize = "16px";
+vali4.style.fontSize = "16px";
+let checkstatus = true;
+createInputs[0].addEventListener("keyup", () => {
+  if (!createInputs[0].value.trim()) {
+    vali1.style.color = "red";
+    vali1.textContent = "Plese enter username correctly";
+    checkstatus *= false;
+  } else {
+    vali1.style.color = "green";
+    vali1.textContent = "Correct validation";
   }
-    return (false)
+});
+createInputs[1].addEventListener("keyup", () => {
+  if (!ValidateEmail(createInputs[1].value.trim())) {
+    vali2.style.color = "red";
+    vali2.textContent = "Please enter valid email";
+    checkstatus *= false;
+  } else {
+    vali2.style.color = "green";
+    vali2.textContent = "Correct validation";
+  }
+});
+
+createInputs[2].addEventListener("keyup", () => {
+  if (!ValidatePassword(createInputs[2].value.trim())) {
+    vali3.style.color = "red";
+    vali3.textContent = "Please enter valid Password";
+    checkstatus *= false;
+  } else {
+    vali3.style.color = "green";
+    vali3.textContent = "Correct validation";
+  }
+});
+
+createInputs[3].addEventListener("keyup", () => {
+  if (createInputs[2].value.trim() != createInputs[3].value.trim()) {
+    vali4.style.color = "red";
+    vali4.textContent = "Please enter valid Password";
+    checkstatus *= false;
+  } else {
+    vali4.style.color = "green";
+    vali4.textContent = "Correct validation";
+  }
+});
+
+vali5.addEventListener("click", () => {
+  const newwtoast = document.createElement("div");
+  const newwp = document.createElement("p");
+  newwtoast.style.width = "250px";
+  newwtoast.style.height = "60px";
+  newwp.style.color = "white";
+  newwp.style.fontSize = "16px";
+  newwtoast.style.display = "flex";
+  newwtoast.style.justifyContent = "center";
+  newwtoast.style.alignItems = "center";
+  newwtoast.style.position = "fixed";
+  document.body.style.position = "relative";
+  newwtoast.style.left = "75vw";
+  newwtoast.style.bottom = "85vh";
+  if (!!checkstatus) {
+    newwtoast.style.backgroundColor = "green";
+    newwp.textContent = "Success";
+  } else {
+    newwtoast.style.backgroundColor = "red";
+    newwp.textContent = "Error";
+  }
+
+  newwtoast.append(newwp);
+  rightbottom.append(newwtoast);
+
+  setTimeout(() => {
+    newwtoast.remove();
+  }, 2000);
+  
+});
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
+
+function ValidatePassword(password) {
+  if (
+    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/.test(
+      password
+    )
+  ) {
+    return true;
+  }
+  return false;
 }
